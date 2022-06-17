966b42480b95d98b09ac6370901073b919a0df3f
AinTEAsports <benkirat.khalil@gmail.com>
2022-06-17 21:14:00 +0200

    main.py : added parser to use it cli

--- main.py
+++ main.py
@@ -14,12 +14,24 @@ import termcolor
 from utils import get_all_files, File
 
 
+
+ascii_art = """
+ dP""b8 88""Yb Yb  dP 88""Yb 888888 Yb        dP    db    88""Yb 888888 
+dP   `" 88__dP  YbdP  88__dP   88    Yb  db  dP    dPYb   88__dP 88__   
+Yb      88"Yb    8P   88\"""    88     YbdPYbdP    dP__Yb  88"Yb  88""   
+ YboodP 88  Yb  dP    88       88      YP  YP    dP"\"""Yb 88  Yb 888888
+"""
+
+print(termcolor.colored(ascii_art, 'green'))
+
+
 def get_home_directory() -> str :
     return str(Path.home())
 
 
 filename = os.path.basename(__file__)
 home_directory = get_home_directory()
+COLORED_STAR = termcolor.colored('*', 'green')
 
 no_encrypt_list = [
     filename,
@@ -30,6 +42,43 @@ parser = argparse.ArgumentParser(
     description="Fun tool to encrypt your WHOLE user directory, so use it carefully"
 )
 
+parser.add_argument(
+    "--encrypt",
+    action="store_true",
+    help="Encrypt your directory"
+)
+
+parser.add_argument(
+    "--decrypt",
+    action="store_true",
+    help="Decrypt your directory"
+)
+
+parser.add_argument(
+    "-k",
+    "--key",
+    type=str,
+    help="Key to encrypt/decrypt files",
+)
+
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Prints all files that are encrypted/decrypted"
+)
+
+args = parser.parse_args()
+
+if (args.encrypt and args.decrypt) or (not args.encrypt and not args.decrypt):
+    warning_text = termcolor.colored("[/!\\] Invalid usage of flags\n", "yellow")
+
+
+action: str = "Encrypting" if args.encrypt else "Decrypting"
 
 for file in get_all_files(home_directory):
-    print(file)
+    if args.verbose:
+        print(f"[{COLORED_STAR}] {action} file {file}...")
+    
+    #File(file).encrypt(args.key)
+    pass
